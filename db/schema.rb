8dd9efa6e220fb22a6da52798bab29a0a5dcb84e
Lucas Vocos <lucas.vocos@me.com>
2015-05-15 13:32:38 -0400

    Add First & Last Name fields to Users, modify login and logout paths

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515161312) do
+ActiveRecord::Schema.define(version: 20150515165639) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20150515161312) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role_id"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
