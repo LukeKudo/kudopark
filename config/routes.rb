5da62735c46b5b6416a57038543be4be3124865f
Luke <Luke@kudopark.com>
2015-05-14 19:13:14 -0500

    added pg and rails_12factor gems for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root "pages#home"
+  root "pages#signin"
   get "about" => "pages#about"
-  get "signin" => "pages#signin"
+  get "admin" => "pages#admin"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
