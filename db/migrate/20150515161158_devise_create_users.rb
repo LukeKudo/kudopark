dafaefc43fb91dec09cc3ea2f008c39b2027764e
Luke <Luke@kudopark.com>
2015-05-16 19:25:56 -0500

    fixed errors for migrate, dropped table command in devise/users

--- db/migrate/20150515161158_devise_create_users.rb
+++ db/migrate/20150515161158_devise_create_users.rb
@@ -1,5 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
+    drop_table :users
     create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -33,10 +34,5 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
-
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150515165639) do
 
   create_table "admins", force: :cascade do |t|
@@ -37,8 +36,6 @@ ActiveRecord::Schema.define(version: 20150515165639) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
-ActiveRecord::Schema.define(version: 20150515041456) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -56,11 +53,8 @@ ActiveRecord::Schema.define(version: 20150515041456) do
     t.integer  "role_id"
     t.string   "first_name"
     t.string   "last_name"
-
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["role_id"], name: "index_users_on_role_id"
 
 end
